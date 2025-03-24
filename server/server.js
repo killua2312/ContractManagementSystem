853c2067646533b5d5d3668abf76a1fa768e8cf8
killua2312 <rahulthota123@gmail.com>
2025-03-25 00:02:50 +0530

    Cors production config

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ const PORT = parseInt(process.env.PORT) || 4000;
 // Initialize Socket.IO
 const io = socket.init(server);
 
-app.use(cors());
+const NODE_ENV = process.env.NODE_ENV || "development";
+if (NODE_ENV === "development") {
+  app.use(cors());
+}
 
 app.use(express.json());
 
